6025495696028c7de7c89b8d724bb71600aa93c2
cntakyidanquah <125611230+cntakyidanquah@users.noreply.github.com>
2023-02-24 11:02:47 +0000

    Add files via upload

--- 6948721.py
+++ 6948721.py
@@ -1,117 +1,52 @@
-import numpy as np
-L = 12 #length of beam in meters
-w = 10 #intensity of load in KN/m
-#Question a
-#Bending moment(M) and shear force(V) at the first end, x=0
-x = 0
-M = (w*(-6*x**2 + 6*L*x-L**2))/12
-V = w*(L/2 - x)
-m= 'The bending moment at x=0 is '
-n= 'the shear force at x=0 is '
-print()
-print('(a.1)' + m + str(M) + ' and ', n + str(V))
-#Bending moment(M) and shear force(V) at the first end, x=L=10
-x = L
-M = (w*(-6*x**2 + 6*L*x-L**2))/12
-V = w*(L/2 - x)
-a= 'The bending moment at x=L is '
-b= 'the shear force at x=L is '
-print()
-print('(a.2)' + m + str(M) +' and ', n + str(V))
-#Question b
-""" 
-When the bending moment is zero, we get an expression x**2 - Lx + L**2/6 = 0
-"""
-#from the expression 
-a = 1
-b = -L
-c = L**2/6
-#Using the Almighty formula the two roots are;
-discriminant = b**2 - 4*a*c
-root_1b = (-b + np.sqrt(discriminant))/2*a
-root_2b = (-b - np.sqrt(discriminant))/2*a
-print()
-print('(b) The points of contra-flexure are {0} and {1}'.format(root_1b,root_2b))
-#Question c
-""" 
-When the shear force is zero, x = L/2
-"""
-x = L/2
-print()
-print('(c) The point at which V=0 is {}'.format(x))
-#Question d
-p = 0
-s = 0.01
-q = L + s
-x = np.arange(p,q,s) 
-M = (w*(-6*x**2 + 6*L*x-L**2))/12
-print()
-print('(d) Using the initialized variable,the bending moment at each step in the array is {0}'.format(M))
-#Question e
-V = w*(L/2 - x)
-print()
-print('(e) The shear force for each step along the span is {}'.format(V))
-#Question f
-"""
-Let the absolute value of the bending moment array be AM
-Also let the minimum AM be m_AM
-"""
-AM = abs(M)
-m_AM = min(AM)
-""" 
-When the bending moment is m_AM, we get an expression x**2 - Lx + (L**2/6)+(2*m_AM)/w = 0
-"""
-#from the above expression 
-a = 1
-b = -L
-c = (L**2/6)+(2*m_AM)/w
-#Using the Almighty formula the two roots are;
-discriminant = b**2 - 4*a*c
-root_1f = (-b + np.sqrt(discriminant))/2*a
-root_2f = (-b - np.sqrt(discriminant))/2*a
-print()
-print('(f) The points along L at which the absolute values of the bending moment array is minimum are {0} and {1}'.format(root_1f,root_2f))
-#Question g
-"""
-Let the relative errors be r_e
-"""
-r_e1 = ((root_1b - root_1f)/root_1b*100) 
-r_e2 = ((root_2f - root_2b)/root_2f*100)
-print()
-print('(g) The relative errors between estimated points of contra-flexure are {0}% and {1}%'.format(r_e1,r_e2))
-#Question h
-"""
-Let the maximum bending moment be max_M and the minimum bending moment be min_M
-"""
-#for the maximum
-max_M = max(M)
-""" 
-When the bending moment is max_M, we get an expression x**2 - Lx + (L**2/6)+(2*max_M)/w = 0
-"""
-a = 1
-b = -L
-c = (L**2/6)+(2*max_M)/w
-#Using the Almighty formula the two roots are;
-discriminant = b**2 - 4*a*c
-root_1 = (-b + np.sqrt(discriminant))/2*a
-root_2 = (-b - np.sqrt(discriminant))/2*a
-print()
-print('(h.1) The points at which the maximum bending moment occur are {0} and {1}'.format(root_1,root_2))
-#for the minimum
-min_M = min(M)
-""" 
-When the bending moment is min_M, we get an expression x**2 - Lx + (L**2//6)+(2*min_M)/w = 0
-"""
-a = 1
-b = -L
-c = (L**2//6)+(2*min_M)/w
-#Using the Almighty formula the two roots are;
-discriminant = b**2 - 4*a*c
-root_1 = (-b - np.sqrt(discriminant))/2*a
-root_2 = (-b + np.sqrt(discriminant))/2*a
-print()
-print('(h.2) The points at which the minimum bending moment occur are {0} and {1}'.format(root_1,root_2))
+#List of cars and their prices
+cars={"Mazda 6":58000,
+      "Maclaren":63000,
+      "Land Cruiser":75000,
+      "Mercedes Benz C63":58500, 
+      "Toyota Rush":61000,
+      "Bugatti Veyron":78000,
+      "Prado V8":52000,
+      "Koenigsegg Trevita":380000,
+      "Tesla Model 4":62000,
+      "Ford 150":75600,
+      "Chevrolet Camaro":25950,
+      "Citroen C5X":57670,
+      "Kia Sportage":70000,
+      "Honda Accord":85600,
+      "Honda Civic":79900,
+      "Mercedes EQE":75950,
+      "Lincoln":39000,
+      "Jeep Wrangler":69800,
+      "lotus Emira":93000000,
+      "Hyundai Sonata":86000,
+      "Honda Pilot":85000,
+      "BMW M4":60975,
+      "Highlander":55600,
+      "Porche":82200,
+      "Rolls Royce":100250,
+      "Bugatti Chiron":95000,
+      "Lamboghini":84000,
+      "Ineos Grenadier":11905, 
+      "Hyundai Ioniq 6":41600,
+      "VW Jetta":35600,
+      "Toyota Hilux":55000,
+      "Range Rover Velar":63600,
+      "Aston Martin DBR22":75000,
+      "Hummer EV":108700,
+      "Tundra":45220,
+      "G Wagon":89632,
+      "Benz S550":85500,
+      "Dacia Jogger":25500,
+      "Golf 2022":42500}
+#Get user input for car name
+car_name=input("Enter the car you want")
+#Check if car name is in the list of cars available
+if car_name in cars:
+    #If car name is present,get its price
+    car_price=cars[car_name]
+    print(f"Tthe price of {car_name}is${car_price}")
+else:
+    #If car name is not present,inform the user
+    print(f"Sorry,{car_name}is not available at the moment")
 
-
-#https://github.com/cntakyidanquah/assignment-3
-#my github account
+#https://github.com/cntakyidanquah
